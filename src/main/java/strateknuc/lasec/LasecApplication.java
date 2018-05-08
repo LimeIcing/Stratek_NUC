ae892268fa920bf8967bae9df4330525077dacac
Lime Icing <emil577b@stud.kea.dk>
2018-05-08 10:52:53 +0200

    initialized project

---
+++ src/main/java/strateknuc/lasec/LasecApplication.java
@@ -0,0 +1,12 @@
+package strateknuc.lasec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LasecApplication.class, args);
+    }
+}
