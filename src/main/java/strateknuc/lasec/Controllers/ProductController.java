9d923a3bdeccf909895dcbab33a0a621cfc01ead
jalapenosbud <jalapenosbud@gmail.com>
2018-05-09 10:41:20 +0200

    added create POST method and repository interface to controller

--- src/main/java/strateknuc/lasec/Controllers/ProductController.java
+++ src/main/java/strateknuc/lasec/Controllers/ProductController.java
@@ -2,18 +2,30 @@ package strateknuc.lasec.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import strateknuc.lasec.Interfaces.ProductRepositoryInterface;
 import strateknuc.lasec.Models.ProductModel;
+import strateknuc.lasec.Models.Repositories.ProductRepository;
 
 @Controller
 public class ProductController {
+
+    private ProductRepositoryInterface repositoryInterface = new ProductRepository();
 
     @RequestMapping(value = "/create", method = RequestMethod.GET)
     public String create(Model model)
     {
         model.addAttribute("productModel", new ProductModel());
         return "/create";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(@ModelAttribute ProductModel productModel)
+    {
+        repositoryInterface.createProduct(productModel);
+        return "redirect:/index";
     }
 
 }
