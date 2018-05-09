b11adaf5578b8b85e2802c03e9c5c9b2a4a3e8ea
jalapenosbud <jalapenosbud@gmail.com>
2018-05-09 14:35:05 +0200

    trying to make status message from product creation success / fail

--- src/main/java/strateknuc/lasec/Models/Repositories/ProductRepository.java
+++ src/main/java/strateknuc/lasec/Models/Repositories/ProductRepository.java
@@ -6,14 +6,19 @@ import org.springframework.stereotype.Repository;
 import strateknuc.lasec.Interfaces.ProductRepositoryInterface;
 import strateknuc.lasec.Models.ProductModel;
 
+import java.sql.SQLException;
+
 @Repository
 public class ProductRepository implements ProductRepositoryInterface {
 
     @Autowired
     private JdbcTemplate jdbc;
 
+    private String returnSuccesfully;
+
     @Override
-    public void createProduct(ProductModel p) {
+    public void createProduct(ProductModel p)
+    {
 
         // Update with SQL tables - sync if anything changes (task)
         String sql = "INSERT INTO products( ean, manufacturer, name, quantity, price, category, description) " +
@@ -26,6 +31,24 @@ public class ProductRepository implements ProductRepositoryInterface {
                 p.getDescription() + "')";
 
             int rowsAffected = jdbc.update(sql);
+            returnSuccesfully = isProductSaved(rowsAffected,p.getName());
             System.out.println("Rows affected: " + rowsAffected);
+    }
+
+    @Override
+    public String isProductSaved(int rowsAffected, String productName) {
+
+        if(rowsAffected == 1) {
+            return productName + "has been created successfully!";
+        }
+        //integrate with SQLException
+        else{
+            return productName + " was not created, try again";
+        }
+    }
+
+    @Autowired
+    public String getReturnSuccesfully() {
+        return returnSuccesfully;
     }
 }
