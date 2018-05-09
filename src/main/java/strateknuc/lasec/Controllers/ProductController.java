0c908540aad9c78ac5dd975bb80df3d4684a9f77
jalapenosbud <jalapenosbud@gmail.com>
2018-05-09 10:35:35 +0200

    updated create html file

--- src/main/java/strateknuc/lasec/Controllers/ProductController.java
+++ src/main/java/strateknuc/lasec/Controllers/ProductController.java
@@ -1,10 +1,19 @@
 package strateknuc.lasec.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import strateknuc.lasec.Models.ProductModel;
 
 @Controller
 public class ProductController {
 
-
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public String create(Model model)
+    {
+        model.addAttribute("productModel", new ProductModel());
+        return "/create";
+    }
 
 }
