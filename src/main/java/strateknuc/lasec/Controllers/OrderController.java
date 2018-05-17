fe5775a1eec54d693f824a431c487af0db93837e
jalapenosbud <jalapenosbud@gmail.com>
2018-05-17 11:46:20 +0200

    order repo and controller and interface

---
+++ src/main/java/strateknuc/lasec/Controllers/OrderController.java
@@ -0,0 +1,7 @@
+package strateknuc.lasec.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+}
---
+++ src/main/java/strateknuc/lasec/Models/Repositories/OrderRepository.java
@@ -0,0 +1,7 @@
+package strateknuc.lasec.Models.Repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRepository {
+}
