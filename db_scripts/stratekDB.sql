36869539be01e5e86b1aae5201c64332ac776b80
Lime Icing <emil577b@stud.kea.dk>
2018-05-23 14:22:54 +0200

    merged view.sql into stratekDB.sql

--- db_scripts/stratekDB.sql
+++ db_scripts/stratekDB.sql
@@ -37,3 +37,37 @@ CREATE TABLE product_orders(
     FOREIGN KEY(order_id) REFERENCES orders(id),
     FOREIGN KEY(product_ean) REFERENCES products(ean)
 );
+
+-- AUTHOR(S): ECS
+CREATE VIEW product_list AS
+SELECT
+	CONCAT(manufacturer,' ' , name) AS name,
+    price,
+    category,
+    ean
+FROM products
+ORDER BY name;
+
+-- AUTHOR(S): ECS
+CREATE VIEW product_list_admin AS
+SELECT
+	ean,
+	CONCAT(manufacturer,' ' , name) AS name,
+    quantity,
+    price,
+    category
+FROM products
+ORDER BY category, name;
+
+-- AUTHOR(S): LKB, ECS
+CREATE VIEW order_list AS
+SELECT
+	product_orders.order_id,
+    date,
+    customer_name,
+    customer_email,
+    product_orders.product_ean,
+    product_orders.quantity
+FROM orders
+JOIN product_orders ON id=product_orders.order_id
+ORDER BY id;
