f4b73186cf19f03fc5a49f5da5e204a0dccbe4da
Lime Icing <emil577b@stud.kea.dk>
2018-05-23 10:35:13 +0200

    cleaned up code in the controllers

--- src/main/java/strateknuc/lasec/Controllers/HomeController.java
+++ src/main/java/strateknuc/lasec/Controllers/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     // AUTHOR(S): AP
     // index.html GET REQUEST
-    // this is called when index is refreshed
+    // This is called when index is refreshed
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index() {
         return "/index";
@@ -26,7 +26,7 @@ public class HomeController {
     }
 
     // AUTHOR(S): ECS
-    // redirects to /admin/index when the "/index" part isn't present in the URL
+    // Redirects to /admin/index when the "/index" part isn't present in the URL
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String adminIndexRedirect() {
         return "/admin/index";
--- src/main/java/strateknuc/lasec/Controllers/ProductController.java
+++ src/main/java/strateknuc/lasec/Controllers/ProductController.java
@@ -21,7 +21,6 @@ import java.sql.SQLIntegrityConstraintViolationException;
 @Controller
 public class ProductController {
 
-    // this finds and creates beans
     // AUTHOR(S): AP, ECS
     @Autowired
     private ProductRepositoryInterface productRepository = new ProductRepository();
@@ -30,22 +29,20 @@ public class ProductController {
 
     // AUTHOR(S): AP, ECS
     // create.html GET REQUEST
-    // this is called when the create.html file gets refreshed
+    // This is called when the create.html file gets refreshed
     @RequestMapping(value = "/admin/create", method = RequestMethod.GET)
     public String create(Model model, Model categoryModel) {
         model.addAttribute("productModel", new ProductModel());
         categoryModel.addAttribute("options", categoryRepository.get());
+
         return "/admin/create";
     }
 
     // AUTHOR(S): AP
     // create.html POST REQUEST
-    // this is called when a form="action" method="POST" is called
-    // i.e when a button gets pressed and sends data further
+    // This is called when you create a new product via the create page
     @RequestMapping(value = "/admin/create", method = RequestMethod.POST)
     public String create(@ModelAttribute ProductModel productModel, RedirectAttributes rdt) throws Exception {
-        // rdt.addFlashAttribute("message", msg);
-
         String msg = "";
 
         try {
@@ -55,8 +52,9 @@ public class ProductController {
         catch (SQLIntegrityConstraintViolationException e) {
             msg = "Product already exists, please try again.";
         }
+
         rdt.addFlashAttribute("message", msg);
-        // redirect is used to switch pages
+
         return "redirect:/admin/index";
     }
 
@@ -65,36 +63,38 @@ public class ProductController {
     public String edit(Model model, @PathVariable(value = "ean") String ean, Model categoryModel) throws Exception {
         model.addAttribute("product", productRepository.get(ean));
         categoryModel.addAttribute("options", categoryRepository.get());
+
         return "/admin/editProduct";
     }
 
     // AUTHOR(S): ECS
     // editProduct.html POST REQUEST
-    // this is called when a form="action" method="POST" is called
-    // i.e when a button gets pressed and sends data further
+    // This is called when a product is edited via the edit page
     @RequestMapping(value = "/admin/editProduct", method = RequestMethod.POST)
     public String edit(@ModelAttribute ProductModel productModel, RedirectAttributes rdt) throws Exception{
         rdt.addFlashAttribute("message", "Vare redigeret");
         productRepository.updateProduct(productModel);
+
         return "redirect:/admin/editList";
     }
 
     // AUTHOR(S): CPS, ECS
-    //Mapping for Delete page
+    // Mapping for Delete page
     @RequestMapping(value = "/admin/delete/{ean}", method = RequestMethod.GET)
     public String delete(Model model, @PathVariable(value = "ean") String ean) throws Exception {
         model.addAttribute("product", productRepository.get(ean));
+
         return "/admin/delete";
     }
 
     // AUTHOR(S): ECS, CPS
     // editProduct.html POST REQUEST
-    // this is called when a form="action" method="POST" is called
-    // i.e when a button gets pressed and sends data further
+    // This is called when you delete a product via the delete confirmation page
     @RequestMapping(value = "/admin/delete/{ean}", method = RequestMethod.POST)
     public String delete(@PathVariable(value = "ean") String ean, RedirectAttributes rdt) throws Exception {
         rdt.addFlashAttribute("message", "Vare slettet");
         productRepository.deleteProduct(ean);
+
         return "redirect:/admin/editList";
     }
 
@@ -102,6 +102,7 @@ public class ProductController {
     @RequestMapping(value = "/product/category/{category}", method = RequestMethod.GET)
     public String productIndex (Model model, @PathVariable(value = "category") String category) throws Exception {
         model.addAttribute("products", productRepository.getList(category));
+
         return "/product/category";
     }
 
@@ -109,14 +110,15 @@ public class ProductController {
     @RequestMapping(value = "/admin/editList", method = RequestMethod.GET)
     public String adminProducts (Model model) throws Exception {
         model.addAttribute("products", productRepository.getAdminList());
+
         return "/admin/editList";
     }
 
     // AUTHOR(S): ECS
     @RequestMapping(value = "/product/details/{ean}", method = RequestMethod.GET)
     public String productDetails(Model model, @PathVariable(value = "ean") String ean) throws Exception {
-        System.out.println(ean);
         model.addAttribute("product", productRepository.get(ean));
+
         return "/product/details";
     }
 }
