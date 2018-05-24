461b8e96216e3c3595d8394712b923a0d97745e3
lass5643 <lass5643@stud.kea.dk>
2018-05-24 12:50:33 +0200

    OrderRepositoryInterface javaDoc commented

--- src/main/java/strateknuc/lasec/Interfaces/OrderRepositoryInterface.java
+++ src/main/java/strateknuc/lasec/Interfaces/OrderRepositoryInterface.java
@@ -4,7 +4,11 @@ import strateknuc.lasec.Models.ProductModel;
 import strateknuc.lasec.Models.ProductOrderModel;
 import java.util.List;
 
-// AUTHOR(S): SS, AP, LKB
+/**
+ * @author SS, AP, LKB
+ * Interface that is implemented in OrderRepository
+ * Check OrderRepository for method descriptions
+ */
 public interface OrderRepositoryInterface {
     void addOrderToDatabase(String customerName, String customerEmail, List<ProductModel> productList);
     List<ProductOrderModel> getOrdersFromDatabase();
