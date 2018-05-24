0600251464093bb2f7fc5a663ee20a9a779ef32d
Lime Icing <emil577b@stud.kea.dk>
2018-05-24 12:56:43 +0200

    added javadoc comments for OrderController.java

--- src/main/java/strateknuc/lasec/Controllers/ProductController.java
+++ src/main/java/strateknuc/lasec/Controllers/ProductController.java
@@ -14,6 +14,9 @@ import strateknuc.lasec.Models.ProductModel;
 import strateknuc.lasec.Models.Repositories.CategoryRepository;
 import strateknuc.lasec.Models.Repositories.ProductRepository;
 
+/**
+ * @author AP, ECS, CPS, LKB
+ */
 @Controller
 public class ProductController {
 
@@ -23,9 +26,13 @@ public class ProductController {
     @Autowired
     private CategoryRepositoryInterface categoryRepository = new CategoryRepository();
 
+    /**
+     * GET method called when you click 'Opret produkt' on the admin page.
+     * @param model Model for holding the empty ProductModel to be filled out in the view.
+     * @param categoryModel List of categories used in the drop-down selector in the view.
+     * @return The mapping for the creation page.
+     */
     // AUTHOR(S): AP, ECS
-    // create.html GET REQUEST
-    // This is called when the create.html file gets refreshed
     @RequestMapping(value = "/admin/create", method = RequestMethod.GET)
     public String create(Model model, Model categoryModel) {
         model.addAttribute("productModel", new ProductModel());
@@ -34,9 +41,13 @@ public class ProductController {
         return "/admin/create";
     }
 
-    // AUTHOR(S): AP
-    // create.html POST REQUEST
-    // This is called when you create a new product via the create page
+    /**
+     * POST method called when you create a new product via the create page.
+     * @param productModel The filled out ProductModel from the form on the create page.
+     * @param rdt A version of Model used to show a message on the page.
+     * @return Redirect mapping for the damin front page.
+     */
+    // AUTHOR(S): AP, ECS
     @RequestMapping(value = "/admin/create", method = RequestMethod.POST)
     public String create(@ModelAttribute ProductModel productModel, RedirectAttributes rdt) {
         String msg = productRepository.createProduct(productModel);
@@ -46,6 +57,13 @@ public class ProductController {
         return "redirect:/admin/index";
     }
 
+    /**
+     * GET method called when clicking the edit button on the list of products on the admin page.
+     * @param model Holds the ProductModel fetched from the DB.
+     * @param ean EAN number used to fetch the correct product from the DB.
+     * @param categoryModel List of categories used in the drop-down selector in the view.
+     * @return The mapping for the edit page.
+     */
     // AUTHOR(S): CPS
     @RequestMapping(value = "/admin/editProduct/{ean}", method = RequestMethod.GET)
     public String edit(Model model, @PathVariable(value = "ean") String ean, Model categoryModel) {
@@ -55,9 +73,13 @@ public class ProductController {
         return "/admin/editProduct";
     }
 
+    /**
+     * POST method called when a product is edited via the edit page.
+     * @param productModel The ProductModel holding the edited values from the edit page.
+     * @param rdt A version of Model used to show a message on the page.
+     * @return Redirect mapping leading back to the list of products on the admin page.
+     */
     // AUTHOR(S): ECS
-    // editProduct.html POST REQUEST
-    // This is called when a product is edited via the edit page
     @RequestMapping(value = "/admin/editProduct", method = RequestMethod.POST)
     public String edit(@ModelAttribute ProductModel productModel, RedirectAttributes rdt) {
         rdt.addFlashAttribute("message", "Vare redigeret");
@@ -66,8 +88,13 @@ public class ProductController {
         return "redirect:/admin/editList";
     }
 
+    /**
+     * GET method called when clicking the delete button on the list of products on the admin page.
+     * @param model Holds the ProductModel fetched from the DB.
+     * @param ean EAN number used to fetch the correct product from the DB.
+     * @return Mapping for the delete confirmation page.
+     */
     // AUTHOR(S): CPS, ECS
-    // Mapping for Delete page
     @RequestMapping(value = "/admin/delete/{ean}", method = RequestMethod.GET)
     public String delete(Model model, @PathVariable(value = "ean") String ean) {
         model.addAttribute("product", productRepository.get(ean));
@@ -75,9 +102,13 @@ public class ProductController {
         return "/admin/delete";
     }
 
+    /**
+     * POST method called when you delete a product via the delete confirmation page
+     * @param ean EAN number of the product to be deleted from the DB.
+     * @param rdt A version of Model used to show a message on the page.
+     * @return Redirect mapping leading back to the list of products on the admin page.
+     */
     // AUTHOR(S): ECS, CPS
-    // editProduct.html POST REQUEST
-    // This is called when you delete a product via the delete confirmation page
     @RequestMapping(value = "/admin/delete/{ean}", method = RequestMethod.POST)
     public String delete(@PathVariable(value = "ean") String ean, RedirectAttributes rdt) {
         rdt.addFlashAttribute("message", "Vare slettet");
@@ -86,6 +117,12 @@ public class ProductController {
         return "redirect:/admin/editList";
     }
 
+    /**
+     * GET method called when you click a product category to browse.
+     * @param model Holds the list of products in the given category.
+     * @param category The category that's been clicked on.
+     * @return Mapping for the list of products in the given category.
+     */
     // AUTHOR(S): LKB, ECS, CPS
     @RequestMapping(value = "/product/category/{category}", method = RequestMethod.GET)
     public String productIndex (Model model, @PathVariable(value = "category") String category) {
@@ -94,6 +131,11 @@ public class ProductController {
         return "/product/category";
     }
 
+    /**
+     * GETE method called when you click 'Redigér produkt' on the admin page.
+     * @param model Holds the list of all products.
+     * @return Mapping for the list of products on the admin page.
+     */
     // AUTHOR(S): ECS
     @RequestMapping(value = "/admin/editList", method = RequestMethod.GET)
     public String adminProducts (Model model) {
@@ -102,6 +144,12 @@ public class ProductController {
         return "/admin/editList";
     }
 
+    /**
+     * GET method called when you click a product somewhere on the customer site.
+     * @param model Holds the ProductModel of the clicked product, fetched from the DB.
+     * @param ean EAN number of the clicked product.
+     * @return Mapping for the product details page.
+     */
     // AUTHOR(S): ECS
     @RequestMapping(value = "/product/details/{ean}", method = RequestMethod.GET)
     public String productDetails(Model model, @PathVariable(value = "ean") String ean) {
